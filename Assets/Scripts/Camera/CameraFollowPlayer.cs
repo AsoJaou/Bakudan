7cc1074538df42b9cc45ca864533fda9e8d5d777
Aaron Zheng <21211@burnside.school.nz>
2025-05-27 13:23:12 +1200

    Added Camera Toggle Action. Able to follow character and stop following
    Mouse control over camera yet to add

--- Assets/Scripts/Camera/CameraFollowPlayer.cs
+++ Assets/Scripts/Camera/CameraFollowPlayer.cs
@@ -1,18 +1,46 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraFollowPlayer : MonoBehaviour
 {
     public Transform Player;
     private Vector3 cameraDistance;
+    private bool cameraFollow;
+
+    private InputAction Y;
 
     private void Start()
     {
+        Y = InputSystem.actions.FindAction("Toggle Camera");
         cameraDistance = transform.position - Player.transform.position;
+        cameraFollow = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Y.WasPressedThisFrame())
+        {
+            cameraFollow = !cameraFollow;
+        }
+
+        if (cameraFollow)
+        {
+            FollowPlayer();
+        }
+        else
+        {
+            FollowMouse();
+        }
+    }
+
+    void FollowPlayer ()
+    {
         transform.position = Player.transform.position + cameraDistance;
+    }
+
+    void FollowMouse ()
+    {
+        return;
     }
 }
