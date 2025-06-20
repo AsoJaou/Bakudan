fd559547e0f7e50d73fec30c135c2641b98b6775
Aaron Zheng <21211@burnside.school.nz>
2025-06-20 17:24:52 +1200

    Lowered Base Move Speed

--- Assets/Scripts/Input/PlayerController.cs
+++ Assets/Scripts/Input/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Move Settings")]
     [SerializeField]
-    private float moveSpeed = 10f;
+    private float baseMoveSpeed = 3f;
 
     //Input Actions
     private InputAction rightClick;
@@ -118,7 +118,7 @@ public class PlayerController : MonoBehaviour
             currentPos = transform.position;
             displacement = target - currentPos;
             direction = displacement.normalized * Time.deltaTime;
-            transform.position += direction * moveSpeed;
+            transform.position += direction * baseMoveSpeed;
 
             yield return null;
         }
@@ -131,7 +131,7 @@ public class PlayerController : MonoBehaviour
             currentPos = transform.position;
             displacement = target.transform.position - currentPos;
             direction = displacement.normalized * Time.deltaTime;
-            transform.position += direction * moveSpeed;
+            transform.position += direction * baseMoveSpeed;
 
             yield return null;
         }
