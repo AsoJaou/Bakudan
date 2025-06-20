d9f20655797e76ba79c7508fd60af07675c7f446
Aaron Zheng <21211@burnside.school.nz>
2025-06-20 16:02:13 +1200

    NormalAttack tracks enemy position (self updates) and destory on hit

--- Assets/Scripts/Player/NormalAttack.cs
+++ Assets/Scripts/Player/NormalAttack.cs
@@ -1,10 +1,32 @@
+using System.Collections;
 using UnityEngine;
 
 public class NormalAttack : MonoBehaviour
 {
-    void AttackTarget()
+    //Normal Attack Variables
+    private float baseAttackSpeed = 10f;
+    private Vector3 currentPos;
+    private Vector3 displacement;
+    private Vector3 direction;
+
+    void AttackTarget(GameObject target)
     {
         Debug.Log("TARGET ATTACKED");
+        StartCoroutine(MoveToPosition(target.transform));
 
+    }
+
+    IEnumerator MoveToPosition(Transform targetPosition)
+    {
+        while (Vector3.Distance(transform.position, targetPosition.position) > 0.1f)
+        {
+            currentPos = transform.position;
+            displacement = targetPosition.position - currentPos;
+            direction = displacement.normalized * Time.deltaTime;
+            transform.position += direction * baseAttackSpeed;
+
+            yield return null;
+        }
+        Destroy(gameObject);
     }
 }
