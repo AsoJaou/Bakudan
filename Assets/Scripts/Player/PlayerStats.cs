2c4572be90f028d817808c806e5912dff4b1c798
Aaron Zheng <21211@burnside.school.nz>
2025-07-15 06:59:44 +1200

    Created global PlayerStats Singleton to store all stats

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -2,15 +2,43 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static PlayerStats Instance { get; private set; }
+
+    public float Speed = 67f;
+    public float NormalAttackSpeed = 15f;
+    public float NormalAttackDamage;
+    public float Health = 600f;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void BonusSpeed(float speed)
     {
-        
+        Speed += speed;
+    }
+
+    public void BonusAttackSpeed(float normalAttackSpeed)
+    {
+        NormalAttackSpeed += normalAttackSpeed;
+    }
+
+    public void BonusNormalAttackDamage(float normalAttackDamage)
+    {
+        NormalAttackDamage += normalAttackDamage;
+    }
+
+    public void BonusHealth(float health)
+    {
+        Health += health;
     }
 }
